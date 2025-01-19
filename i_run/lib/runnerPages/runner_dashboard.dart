760aacdbc3a47c5ce2664cbc27e4cf497edfcf9a
Faris Aiman <farisaimanCS@protonmail.com>
2025-01-19 20:46:03 +0800

    Initial runner and student dashboard

--- i_run/lib/runnerPages/runner_dashboard.dart
+++ i_run/lib/runnerPages/runner_dashboard.dart
@@ -10,6 +10,78 @@ class runnerDashboard extends StatefulWidget {
 class _runnerDashboardState extends State<runnerDashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "IIUM ERRAND RUNNER",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color.fromARGB(255, 8, 164, 92),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Dashboard', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Container(
+                width: 400,
+                height: 400,
+                color: Color.fromARGB(255, 8, 164, 92),
+                // This one for inside the containers
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                      Container(
+                        height: 150,
+                        width: 150,
+                        color: Colors.white,
+                        child: Text('Available Tasks'),
+                      ),
+                      Container(
+                        height: 150,
+                        width: 150,
+                        color: Colors.white,
+                        child: Text('Task Progress'),
+                      )
+                    ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          color: Colors.white,
+                          child: Text('Task history'),
+                        ),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          color: Colors.white,
+                          child: Text('Personal Information'),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+          ),
+          Text('Notifications', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          Container(
+            width: 300,
+            color: Color.fromARGB(255, 8, 196, 236),
+            child: Text(
+              'Yay! new task found! Check "Available Tasks" to accept.',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- i_run/lib/studentPages/student_dashboard.dart
+++ i_run/lib/studentPages/student_dashboard.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class studentDashboard extends StatefulWidget {
@@ -12,7 +14,75 @@ class _studentDashboardState extends State<studentDashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Student Dashboard"),
+        title: Text(
+          "IIUM ERRAND RUNNER",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color.fromARGB(255, 8, 164, 92),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Dashboard', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Container(
+                width: 400,
+                height: 400,
+                color: Color.fromARGB(255, 8, 164, 92),
+                // This one for inside the containers
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                      Container(
+                        height: 150,
+                        width: 150,
+                        color: Colors.white,
+                        child: Text('Assign Errand'),
+                      ),
+                      Container(
+                        height: 150,
+                        width: 150,
+                        color: Colors.white,
+                        child: Text('Errand Progress'),
+                      )
+                    ]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          color: Colors.white,
+                          child: Text('Apply as runner'),
+                        ),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          color: Colors.white,
+                          child: Text('Personal Information'),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+          ),
+          Text('Notifications', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          Container(
+            width: 300,
+            color: Color.fromARGB(255, 8, 196, 236),
+            child: Text(
+              'Yay! We have found you a runner. Check "Errand Progress" to check status',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
       ),
     );
   }
